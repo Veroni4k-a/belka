a930c18a8af19b5cd40d50dc001c881c88709a49
Veronika <153688137+Veroni4k-a@users.noreply.github.com>
2025-10-28 13:09:09 +0300

    Create esp32_motors.py
    Через While True

---
+++ driver_motors/esp32_motors.py
@@ -0,0 +1,55 @@
+from machine import Pin, PWM
+import time
+
+# Пины для управления моторами
+FwdPin_A = 10
+BwdPin_A = 9
+FwdPin_B = 6
+BwdPin_B = 5
+
+MaxSpd = 100  # Скорость, значение 0-100 (в MicroPython обычно 0-65535)
+
+# Создаем объекты PWM для управления скоростью
+fwd_a = PWM(Pin(FwdPin_A))
+bwd_a = PWM(Pin(BwdPin_A))
+fwd_b = PWM(Pin(FwdPin_B))
+bwd_b = PWM(Pin(BwdPin_B))
+
+# Настраиваем частоту ШИМ (опционально)
+fwd_a.freq(1000)
+bwd_a.freq(1000)
+fwd_b.freq(1000)
+bwd_b.freq(1000)
+
+def stop_all():
+    """Остановить все моторы"""
+    fwd_a.duty_u16(0)
+    bwd_a.duty_u16(0)
+    fwd_b.duty_u16(0)
+    bwd_b.duty_u16(0)
+
+# Бесконечный цикл
+while True:
+    # Движение двигателем A вперед
+    bwd_a.duty_u16(0)
+    fwd_a.duty_u16(MaxSpd * 65535 // 100)  # Конвертируем 0-100 в 0-65535
+    time.sleep(5)
+    fwd_a.duty_u16(0)
+    
+    # Движение двигателем A назад
+    bwd_a.duty_u16(MaxSpd * 65535 // 100)
+    fwd_a.duty_u16(0)
+    time.sleep(5)
+    bwd_a.duty_u16(0)
+    
+    # Движение двигателем B вперед
+    bwd_b.duty_u16(0)
+    fwd_b.duty_u16(MaxSpd * 65535 // 100)
+    time.sleep(5)
+    fwd_b.duty_u16(0)
+    
+    # Движение двигателем B назад
+    bwd_b.duty_u16(MaxSpd * 65535 // 100)
+    fwd_b.duty_u16(0)
+    time.sleep(5)
+    bwd_b.duty_u16(0)
